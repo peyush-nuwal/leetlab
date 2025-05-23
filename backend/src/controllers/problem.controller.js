66c9e957f60fe08b8d35874773c8b85493c382be
Nihal <nihal7u@gmail.com>
2025-05-23 13:27:09 +0530

    Implemented createProblem controller

--- backend/src/controllers/problem.controller.js
+++ backend/src/controllers/problem.controller.js
@@ -1,5 +1,56 @@
+import {db} from "../libs/db.js"
+import { getJudge0LanguageId, poolBatchResults, submitBatch } from "../libs/judge0.lib.js";
+
 export const createProblem = async (req, res) => {
-    
+    const {title, description, difficulty, tags, examples, constraints, testcases, codeSnippets, referenceSolutions} = req.body;
+
+    if (req.user !== "ADMIN") {
+        return res.status(403).json({error: "You are not allowed to create a problem"})
+    }
+
+    try {
+        for(const [language, solutionCode] of Object.entries(referenceSolutions)) {
+            const languageId = getJudge0LanguageId(language);
+
+            if (!languageId) {
+                return res.status(400).json({error: `Language ${language} is not supported`})
+            }
+        }
+
+        const submissions = testcases.map(({input, output})=>({
+            source_code: solutionCode,
+            language_id: languageId,
+            stdin: input, 
+            expected_output: output
+        }))
+
+        const submisssionResults = await submitBatch(submissions);
+        
+        const tokens = submisssionResults.map((res)=>res.token);
+
+        const results = await poolBatchResults(tokens);
+
+        for (let i=0; i<results.length; i++) {
+            const result = results[i];
+
+            if (result.status.id !== 3) {
+                return res.status(400).json({error :`Testcase ${i+1} failed for language ${language}`})
+            }
+
+            // save problem in database
+            const newProblem = await db.problem.create({
+                data: {
+                    title, description, difficulty, tags, examples, constraints, testcases, codeSnippets, referenceSolutions, userId: req.user.id
+                }
+            })
+
+            return res.status(201).json(newProblem);
+        }
+
+    } catch (error) {
+        
+    }
+
 }
 
 export const getAllProblems = async (req, res) => {
@@ -21,5 +72,4 @@ export const deleteProblem = async (req, res) => {
 export const getAllProblemsSolvedByUser = async (req, res) => {
 
 }
-
 
