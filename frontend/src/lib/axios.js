c69d68711ee91dcc44822111d5ed010199143ad2
Nihal <nihal7u@gmail.com>
2025-06-08 22:40:19 +0530

    Changed baseURL in axios instance

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://leetlab-api.onrender.com/" : "/api/v1",
+  baseURL: import.meta.env.MODE === "https://leetlab-api.onrender.com/",
   withCredentials: true,
 });
 
