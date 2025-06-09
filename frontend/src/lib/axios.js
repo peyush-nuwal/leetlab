2224d963cb67f8ebdc1e4783bd57d5aacb7cadbd
Nihal <nihal7u@gmail.com>
2025-06-09 13:26:53 +0530

    Reverted

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api/v1" : "https://leetlab-drab.vercel.app/api/v1",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api/v1" : "https://leetlab-api.onrender.com/api/v1",
   withCredentials: true,
 });
 
