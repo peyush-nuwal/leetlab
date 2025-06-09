02e07beeda85a74b154ed508b3286e65ac3733d4
Nihal <nihal7u@gmail.com>
2025-06-09 13:08:07 +0530

    Reverted back

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000
 
 app.use(
     cors({
-      origin: "",
+      origin: ["http://localhost:5173", "https://leetlab-drab.vercel.app"],
       credentials: true,
     })
   );
