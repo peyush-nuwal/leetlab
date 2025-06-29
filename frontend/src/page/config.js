a0ee8564bc1b2dc12d42a547fec7265aeb883391
Nihal <nihal7u@gmail.com>
2025-06-29 19:00:18 +0530

    Updated mode based url

--- frontend/src/page/config.js
+++ frontend/src/page/config.js
@@ -1,1 +1,5 @@
-export const GOOGLE_AUTH_URL="http://localhost:3000/api/v1/auth/google"
+// src/config.js
+export const GOOGLE_AUTH_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api/v1/auth/google"
+    : "https://your-api.onrender.com/api/v1/auth/google";
